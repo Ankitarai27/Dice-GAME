2993f20766af12aa2903a94b8d7292062a9096b6
Ankita Rai <129681815+Ankitarai27@users.noreply.github.com>
2025-03-23 11:26:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-var randomImageSource = "../images/dice" + randomNumber1 + ".png";
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);//set-1 image acc to random no.
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "../images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);//set-2 image aT randon no.
 
 
